7fe8c482f3c4e64bd53ab523f4117b4bc11e868d
SofiiaKor <68386609+SofiiaKor@users.noreply.github.com>
2021-10-17 22:02:30 +0300

    Add files via upload

---
+++ Pr7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pr7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string text = "I go to school. Girl runs to school";
+            var task1 = new Task1();
+            var convertedStr = task1.SetVocabulary(text);
+            Console.WriteLine(convertedStr);
+        }
+    }
+}
---
+++ Pr7/Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace Pr7
+{
+    class Task1
+    {
+        private Dictionary<string, string> _vocabulary = new Dictionary<string, string>();
+
+        public string SetVocabulary(string text)
+        {
+            _vocabulary.Add("I", "boy");
+            _vocabulary.Add("go", "run");
+            _vocabulary.Add("to", "to");
+            _vocabulary.Add("school", "cinema");
+
+            var convertedStr = "";
+            var sentences = text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var words = new List<string>();
+
+            foreach (var t in sentences)
+            {
+                words.AddRange(t.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            for (var i = 0; i < words.Capacity; i++)
+            {
+                if (_vocabulary.ContainsKey(words[i]))
+                {
+                    words[i] = _vocabulary[$"{words[i]}"];
+                }
+                else
+                {
+                    Console.WriteLine($"Enter substitute for {words[i]}: ");
+                    words[i] = Console.ReadLine();
+                }
+
+                convertedStr += words[i];
+            }
+
+            return convertedStr;
+        }
+
+    }
+}
