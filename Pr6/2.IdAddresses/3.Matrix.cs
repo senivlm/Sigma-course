457feaa8f07a1f44c7a4b232fdad421ab200b3d2
SofiiaKor <68386609+SofiiaKor@users.noreply.github.com>
2021-10-18 21:17:18 +0300

    Add files via upload

---
+++ Pr6/2.IdAddresses/3.Matrix.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace Pr6
+{
+    class _3 : IEnumerable
+    {
+        private double[,] _arr2D;
+
+        public double[,] Arr2D
+        {
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                _arr2D = (double[,])value.Clone();
+            }
+            get => (double[,])_arr2D.Clone();
+        }
+
+        public _3() => _arr2D = null;
+
+        public _3(double[,] matrix) => _arr2D = matrix;
+
+        public override string ToString()
+        {
+            var test = "";
+            for (var i = 0; i < _arr2D.GetLength(0); i++)
+            {
+                for (var j = 0; j < _arr2D.GetLength(1); j++)
+                {
+                    test += $"{_arr2D[i, j].ToString(CultureInfo.InvariantCulture):F00} ";
+                }
+                test += "\n";
+            }
+
+            return test;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+
+        public class Enumerator : IEnumerator
+        {
+            private readonly double[,] _mArr2D;
+
+            private int _i;
+            private int _j;
+
+            public Enumerator(_3 m)
+            {
+                _mArr2D = m.Arr2D;
+                _i = _mArr2D.GetLength(0) - 1;
+                _j = _mArr2D.GetLength(1);
+            }
+
+            object IEnumerator.Current => _mArr2D[_i, _j];
+
+            public bool MoveNext()
+            {
+                _j--;
+                if (_j >= 0) return _i >= 0;
+                _j = _mArr2D.GetLength(1) - 1;
+                _i--;
+                return _i >= 0;
+            }
+
+            public void Reset()
+            {
+                _i = _mArr2D.GetLength(0) - 1;
+                _j = _mArr2D.GetLength(1);
+            }
+        }
+    }
+}
--- Pr6/2.IdAddresses/Program.cs
+++ Pr6/2.IdAddresses/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Pr6
 {
@@ -6,7 +8,30 @@ namespace Pr6
     {
         static void Main(string[] args)
         {
-            var attendance = new WebsiteAttendance();
+            //var attendance = new WebsiteAttendance();
+            //Console.WriteLine(attendance);
+
+            
+            //var test = new double[5,3];
+            //var rand = new Random();
+            //for (var i = 0; i < test.GetLength(0); i++)
+            //{
+            //    for (var j = 0; j < test.GetLength(1); j++)
+            //    {
+            //        test[i, j] = Convert.ToDouble($"{(rand.NextDouble() * 10):F00}");
+            //        Console.Write(test[i, j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+            ////var task3 = new _3(test);
+            //var enumerableThing = test;
+
+            //foreach (var num in test.Reverse())
+            //{
+            //    Console.Write($"{num} ");
+            //}
 
         }
     }
